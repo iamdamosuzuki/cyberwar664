07eb1bf011a48aa1e13dfb391c1947405438393e
Nabil Kashyap <n@nabilk.com>
2013-04-04 12:34:12 -0400

    adding nyt test folder

---
+++ nyt_api_test/header.php
@@ -0,0 +1,41 @@
+<?php // SI 664 Nabil Kashyap midterm 2.25.13
+echo <<<_OUT
+<head>
+<title>CYBER NYT API TEST</title>
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+ <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+<style>
+body, #button{
+font-family: "helvetica neue", helvetica, sans-serif;
+font-size: 14px;
+color: black;}
+body {margin: 100px 150px;}
+a{text-decoration: none; color:#000;}
+h1{width: 25%; border-bottom: 1px blue dashed;}
+table{border: 1px solid;  border-collapse: collapse; text-align: center;}
+tr, td{border: 1px solid; padding: 5px; vertical-align: bottom;}
+#button{font-size:14px; background: none; border:none; border-bottom:1px blue dashed;}
+</style>
+ <script>
+jQuery(document).ready(function($) {
+
+$( "#names" ).autocomplete({
+
+	source:'search.php',
+	'open': function(e, ui) {
+	            $('.ui-autocomplete').css('top', $("ul.ui-autocomplete").cssUnit('top')[0] + 4);
+	            $('.ui-autocomplete').css('left', $("ul.ui-autocomplete").cssUnit('left')[0] - 2);
+	        }
+	    }).data( "autocomplete" )._renderItem = function( ul, item ) {
+	            return $( "<li></li>" ).data( "item.autocomplete", item ).append( "<a>" + item.name + "</a>" ).appendTo( ul );
+
+};
+});
+
+</script>
+</head>
+<body>
+_OUT;
+
+?>
---
+++ nyt_api_test/index.php
@@ -0,0 +1,208 @@
+<?php // SI 664 Nabil Kashyap Cyber Test
+
+
+session_start();
+require_once 'login.php';
+
+$_SESSION['status'] = (isset($_SESSION['status'])) ? $_SESSION['status'] : '';
+
+session_destroy(); 
+
+define('API_KEY', '4f7c037903eb76bfe8cf733b608c4478:11:49914020');
+define('API_URL', 'http://api.nytimes.com/svc/search/v1/article?');
+
+// if (isset($_POST['search'])){
+    
+    
+//     if((!$_POST['name'])){
+    
+//         $_SESSION['status'] = 4;
+//         header('Location: index.php');
+//         return;
+        
+//     }else if(is_numeric($_POST['name'])){
+    
+//         $_SESSION['status'] = 7;
+//         header('Location: index.php');
+//         return;
+        
+//     }else{
+
+//         $name = mysql_real_escape_string($_POST['name']);
+        
+//         $query = "INSERT INTO author_list(name) VALUES
+//             ('$name')";
+        
+//         mysql_query($query)
+//             or die("insert failed " . mysql_error());
+        
+//         $_SESSION['status'] = 1;
+//         header('Location: index.php');
+//         return;
+// }}
+
+if (isset($_POST['search'])){
+      $search = mysql_real_escape_string($_POST['search']);
+      header('Location: index.php');
+      
+      $a = 0;
+      $b = 10;
+
+      $ch = curl_init();
+
+      while ($a < 10){
+        $params = array(
+            'api-key' => API_KEY,
+            'query' => "$search",
+            'offset' => $a,
+            'fields' => 'date, byline, nytd_byline, title, nytd_title, url',
+            );
+
+        print_r($params);
+        $a++;
+
+        curl_setopt($ch, CURLOPT_URL, API_URL . http_build_query($params));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_GETFIELDS, $data);
+
+        $inf = curl_exec($ch);
+        }
+
+      curl_close($ch);
+
+      $output = json_decode($inf, true);
+
+      for($i = 0; $i < count($output['results']); $i++){
+        // print_r($output['results'][$i]); echo "<br /><br />";}
+        mysql_insert_array('Articles', $output['results'][$i]);}
+      return;
+      }
+if (isset($_POST['clear'])){
+      mysql_query('TRUNCATE TABLE Articles');
+      $_SESSION['status'] = 2;
+      header('Location: index.php');
+      return;
+}
+
+include_once 'header.php';
+
+ 
+echo "<h1>NYT API TEST DATABASE</h1>";
+
+// shows the right message based on the $_SESSION variable then gets rid of the variable
+switch($_SESSION['status']){
+
+case 1: echo "<h2 style = 'color:green'>name added</h2>"; break;
+case 2: echo "<h2 style = 'color:red'>table cleared</h2>"; break;
+case 3: echo "<h2 style = 'color:green'>name updated</h2>"; break;
+case 4: echo "<h2 style = 'color:red'>all values required</h2>"; break;
+
+}
+
+// $url = 'http://api.nytimes.com/svc/search/v1/article?format=json&query=cyberwarfare&api-key=4f7c037903eb76bfe8cf733b608c4478:11:49914020';
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, $url);
+// $rs = curl_exec($ch);
+
+echo <<<_OUT
+<h2>search</h2>
+<form method='post'>
+<input  type='text' name='search'><br /><br /> 
+<input id='button' type='submit' value='search'>
+</form>
+<form method='post'>
+<input type='hidden' name='clear' value='true'>
+<input id='button' type='submit' name='clear' value='clear table'>
+</form>
+_OUT;
+
+
+
+
+function mysql_insert_array($table, $data, $exclude = array()) {
+
+    $fields = $values = array();
+
+    if( !is_array($exclude) ) $exclude = array($exclude);
+
+    foreach( array_keys($data) as $key ) {
+        if( !in_array($key, $exclude) ) {
+            $fields[] = "$key";
+            $values[] = "'" . mysql_real_escape_string($data[$key]) . "'";
+        }
+    }
+
+    $fields = implode(",", $fields);
+    $values = implode(",", $values);
+
+    if( mysql_query("INSERT INTO $table ($fields) VALUES ($values)") ) {
+        return array( "mysql_error" => false,
+                      "mysql_insert_id" => mysql_insert_id(),
+                      "mysql_affected_rows" => mysql_affected_rows(),
+                      "mysql_info" => mysql_info()
+                    );
+    } else {
+        print_r(array( "mysql_error" => mysql_error() ));
+    }
+
+}
+
+// builds the table based on the database data
+echo "<table><tr><th></th><th></th></tr>";
+
+$query = "SELECT * FROM Articles";
+$result = mysql_query($query);
+if($result){ $rows = mysql_num_rows($result);
+
+  for ($i = 0; $i < $rows ; ++$i){
+      $row = mysql_fetch_row($result);
+      echo "<tr>";
+      for ($j = 0; $j < count($row); ++$j) echo "<td>$row[$j]</td>";
+      // echo '<td><a href="edit.php?id=' . htmlentities($row[2]) .'">edit</a> /';
+      // echo ' <a href="delete.php?id=' . htmlentities($row[2]) . '">delete</a></td>';
+  }}
+
+echo "</table>";
+
+// foreach($output['results'][$i] as $key=>$item){
+//     echo $key . ": "; print_r($item);
+//     echo "<br /><br />";
+//     $q1 = "INSERT INTO Articles($key) VALUES('$item') WHERE ";
+//     // mysql_query($q1);
+//     // unset($q1);
+//     }} 
+
+
+  // $query = "INSERT INTO geodata(date, byline, nytd_byline, title, nytd_title, des_facet, nytd_des_facet, url) 
+  //         VALUES ('$location','$lat','$lng')";
+
+  // $result = curl_exec($ch);
+  // $info = curl_getinfo($ch);
+  // curl_close($ch);
+  // $data = json_decode($result, true);
+  // print_r($data);
+
+// $n = 10;
+// $i = 0;
+// do{
+//   $start = $i * $n;
+//   print "$start\n";
+  
+//   $params = array(
+//     'api-key' => API_KEY,
+//     'query' => 'des_facet:[SCIENCE AND TECHNOLOGY]',
+//     'offset' => $i,
+//     'fields' => 'date,byline,url,des_facet',
+//     );
+//   $data = file_get_contents(API_URL . '/article?' . http_build_query($params));
+//   print_r($data);
+
+//   // foreach ($data->results as $item){}
+//   //   echo json_decode($item, JSON_PRETTY_PRINT);}   
+//     // $jdata = json_encode($item, JSON_PRETTY_PRINT);    // file_put_contents(sprintf('articles/%s.js', md5($item->url)), json_encode($item)); 
+//     // echo $jdata;}
+
+//   sleep(1);
+// } while (++$i < 5);
+
+?>
