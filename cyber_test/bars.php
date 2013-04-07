d0bc3e2d316229e043c795af79560f83b3cc706e
iamdamosuzuki <iownafordprobe@gmail.com>
2013-04-07 18:05:05 -0400

    bars

--- cyber_test/bars.php
+++ cyber_test/bars.php
@@ -24,14 +24,32 @@ $count[] = (int)mysql_result($resultActor, 0);
 $count[] = (int)mysql_result($resultAttack, 0);
 $count[] = (int)mysql_result($resultTech, 0);
 
+$name[] = 'Authors';
+$name[] = 'Experts';
+$name[] = 'Articles';
+$name[] = 'Actors';
+$name[] = 'Attacks';
+$name[] = 'Tech';
+
+$color[] = "rgb(0,114,187)";
+$color[] = "rgb(217,61,4)";
+$color[] = "rgb(0,83,100)";
+$color[] = "rgb(191,107,4)";
+$color[] = "rgb(0,36,59)";
+$color[] = "rgb(160,173,50)";
+
+
 // $nodes = "{    \nodes\: " . $nodes .",    \links\:        [        ]}";
 //$data["bars"] = $bars;
 
 //foreach ($bars as $items)
 //$id[] = $items['id'];
 
+$data["count"] = $count;
+$data["name"] = $name;
+$data["color"] = $color;
 
-$dataJSON = json_encode($count);
+$dataJSON = json_encode($data);
 $jsonobj = json_decode($dataJSON, TRUE);
 
 echo $dataJSON;
