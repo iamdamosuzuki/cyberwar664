89545f25110837d995a285a32d58c24057ea98da
Nabil Kashyap <n@nabilk.com>
2013-04-17 01:38:55 -0400

    some nonsense

--- cyberarchive_dev/index.php
+++ cyberarchive_dev/index.php
@@ -1,16 +1,18 @@
-<?PHP
-include 'util.php'
+<?php 
+include 'util.php';
+include 'barchart.php';
 ?>
 
-<body>
 <h1>THE CYBERARCHIVE</h1>
 <a href='new_article.php'>New Article</a><br/>
 <a href='view_articles.php'>View/Edit Article List</a><br/>
-<a href='authority.php?table=authors_list'>Edit Author List</a><br/>
-<a href='authority.php?table=techs_list'>Edit Tech List</a><br/>
-<a href='authority.php?table=experts_list'>Edit Expert List</a><br/>
-<a href='authority.php?table=attacks_list'>Edit Attack List</a><br/>
-<a href='authority.php?table=actors_list'>Edit Actor List</a><br/><br/>
-<a href='admin.php'>Admin Page</a><br/>
+<a href='authority.php?table=author_list'>Edit Author List</a><br/>
+<a href='authority.php?table=tech_list'>Edit Tech List</a><br/>
+<a href='authority.php?table=expert_list'>Edit Expert List</a><br/>
+<a href='authority.php?table=attack_list'>Edit Attack List</a><br/>
+<a href='authority.php?table=actor_list'>Edit Actor List</a><br/>
+<a href='network.php'>Network Graph</a><br/>
+<a href='inbox.php'>Inbox</a><br/>
 
 </body>
+</html>
--- cyberarchive_dev/util.php
+++ cyberarchive_dev/util.php
@@ -2,6 +2,116 @@
 // This document sets up the database connection and pulls variables into the database from config.php.
 
      include'static/config.php';
+     echo <<<_OUT
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>SI664 Cyberwarfare Database</title>
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+<link rel="stylesheet" href="cyberarchive_back.css" />
+<script type="text/javascript" src="d3/d3.v3.min.js"></script>
+<style type="text/css">
+    svg{
+        float: right;
+        left-margin: 25%;
+    }
+
+    svg.network{float:left;}
+    div.bar {
+        display: inline-block;
+        width: 20px;
+        height: 75px;
+        background-color: teal;
+        margin-right: 2px;
+    }
+    
+    #button {
+        position: absolute;
+        top: 10px;
+        left: 400px;
+    }
+    
+    .btn-mini {
+        padding: 2px 6px;
+        font-size: 11px;
+        line-height: 14px;
+    }
+    .node {
+      stroke: #fff;
+      stroke-width: 1.5px;
+    }
+
+    .link {
+      stroke: #999;
+      stroke-opacity: .6;
+    }
+
+    .node text {
+      pointer-events: none;
+      font: 10px sans-serif;
+      fill: #000;
+      stroke: none;
+    }
+    
+
+</style>        
+
+<script>
+    jQuery.fn.filterByText = function(textbox, selectSingleMatch) {
+        return this.each(function() {
+            var select = this;
+            var options = [];
+            $(select).find('option').each(function() {
+                options.push({value: $(this).val(), text: $(this).text()});
+            });
+            $(select).data('options', options);
+            $(textbox).bind('change keyup', function() {
+                var options = $(select).empty().data('options');
+                var search = $(this).val().trim();
+                var regex = new RegExp(search,"gi");
+              
+                $.each(options, function(i) {
+                    var option = options[i];
+                    if(option.text.match(regex) !== null) {
+                        $(select).append(
+                           $('<option>').text(option.text).val(option.value)
+                        );
+                    }
+                });
+                if (selectSingleMatch === true && $(select).children().length === 1) {
+                    $(select).children().get(0).selected = true;
+                }
+            });            
+        });
+    };
+
+    $(function() {
+        $('#authorSelect').filterByText($('#authorBox'), true);
+    });
+
+    $(function() {
+        $('#attackSelect').filterByText($('#attackBox'), true);
+    });
+
+    $(function() {
+        $('#actorSelect').filterByText($('#actorBox'), true);
+    });
+
+    $(function() {
+        $('#expertSelect').filterByText($('#expertBox'), true);
+    });
+
+    $(function() {
+        $('#techSelect').filterByText($('#techBox'), true);
+    });
+
+</script>
+</head>
+<body>
+_OUT;
+
      echo "<link rel='stylesheet' type='text/css' media='all' href='static/cyberarchive_back.css'/>";
      echo "<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'></script>";
      echo "<script type='text/javascript' src='static/moveFunction2.js'></script>";
